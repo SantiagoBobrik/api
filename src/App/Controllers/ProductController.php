b75c43508d07bf9894170b6caf9d74e0ae9cdd5a
SantiagoDiaz <santiago.bobrik@gmail.com>
2020-10-24 17:38:42 -0300

    Mejoras en JsonResponse.php

--- src/App/Controllers/ProductController.php
+++ src/App/Controllers/ProductController.php
@@ -8,12 +8,13 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 
+
 class ProductController
 {
 
     public function test($request = null, $response = null, array $args = null)
     {
-        $data = Product::find(1);
-        return JsonResponse::withJson($response, $data, 200);
+
+        return JsonResponse::withJson($response, 204);
     }
 }
--- src/App/Helper/JsonResponse.php
+++ src/App/Helper/JsonResponse.php
@@ -1,21 +1,89 @@
 <?php
 
 
-namespace App\Helper;
+namespace Helper;
 
 use Psr\Http\Message\ResponseInterface as Response;
 
-final class JsonResponse
+
+class JsonResponse
 {
+
+    private static $statusCodesAxon =  [200,201,401,403,404,500,503];
+
     public static function withJson(
         Response $response,
-        string $data,
-        int $status = 200
+        int $status = 200,
+        $data = null
     ): Response {
-        $response->getBody()->write($data);
+
+
+
+        $status = self::validateStatusCode($status);
+        $dataResponse =  self::makeResponseMessage($status,$data);
+        $response->getBody()->write($dataResponse);
 
         return $response
             ->withHeader('Content-Type', 'application/json')
             ->withStatus($status);
+    }
+
+    private static function makeResponseMessage($status,$data){
+
+        switch ($status) {
+            case 200:
+                $message = $data ? json_encode($data)  : self::makeObjectSuccess("Success");
+                return $message;
+                break;
+            case 201:
+                $message = $data ? json_encode($data) : self::makeObjectSuccess("Created");
+                return $message;
+                break;
+                $message = $data ? $data : "Not Found";
+                return  self::makeObjectResponseMessage($message);
+                break;
+            case 401:
+                $message = $data ? $data : "Unauthorized";
+                return self::makeObjectResponseMessage($message);
+                break;
+            case 403:
+                $message = $data ? $data : "Forbidden";
+                return self::makeObjectResponseMessage($message);
+                break;
+            case 404:
+                $message = $data ? $data : "Not Found";
+                return self::makeObjectResponseMessage($message);
+                break;
+            case 500:
+                $message = $data ? $data : "Server Error";
+                return self::makeObjectResponseMessage($message);
+                break;
+            case 503:
+                $message = $data ? $data : "Service Unavailable";
+                return self::makeObjectResponseMessage($message);
+                break;
+        }
+
+
+    }
+
+
+    private static function makeObjectResponseMessage(string  $message)
+    {
+        return  json_encode(["error" => $message, "timestamp" => date("h:i:sa")]);
+    }
+
+
+    private static function makeObjectSuccess(string  $message)
+    {
+        return  json_encode(["message" => $message, "timestamp" => date("h:i:sa")]);
+    }
+
+    private static  function validateStatusCode($status){
+
+        if(!in_array($status, self::$statusCodesAxon))
+            return 200;
+
+        return $status;
     }
 }
